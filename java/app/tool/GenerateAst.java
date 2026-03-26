c3d5abf8234c47602319ff7fd51e8ecb07092188
sp41414 <134581084+sp41414@users.noreply.github.com>
2026-03-26 20:27:20 +0400

    feat: syntax tree expressions

---
+++ java/app/tool/GenerateAst.java
@@ -0,0 +1,60 @@
+package org.squirrelang.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+                "Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator, Expr right"));
+    }
+
+    private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+        String path = outputDir + "/" + baseName + ".java";
+        PrintWriter fileWriter = new PrintWriter(path, "UTF-8");
+
+        fileWriter.println("package org.squirrelang;");
+        fileWriter.println();
+        // fileWriter.println("import java.util.List;");
+        // fileWriter.println();
+        fileWriter.println("abstract class " + baseName + "{ ");
+        for (String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+
+            defineTypes(baseName, fileWriter, className, fields);
+        }
+        fileWriter.println("}");
+        fileWriter.close();
+    }
+
+    private static void defineTypes(String baseName, PrintWriter fileWriter, String className,
+            String fields) {
+        fileWriter.println("  static class " + className + " extends " + baseName + "{ ");
+        fileWriter.println("    " + className + "(" + fields + ") { ");
+
+        String[] fieldList = fields.split(", ");
+        for (String field : fieldList) {
+            // skip the type definition
+            field = field.split(" ")[1];
+            fileWriter.println("      this." + field + " = " + field + ";");
+        }
+        fileWriter.println("    }");
+
+        fileWriter.println();
+        for (String field : fieldList) {
+            fileWriter.println("    final " + field + ";");
+        }
+        fileWriter.println("  }");
+    }
+}
