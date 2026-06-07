5aa85f7ebc8369eb5fb8a6aafcafafdd3e7eb7fc
sp41414 <134581084+sp41414@users.noreply.github.com>
2026-06-08 01:56:34 +0400

    feat: basic vm execution & fix debugger long constant

--- c/src/debug.c
+++ c/src/debug.c
@@ -26,8 +26,8 @@ static int constantInstruction(const char *name, Chunk *chunk, int offset) {
 }
 
 static int longConstantInstruction(const char *name, Chunk *chunk, int offset) {
-  uint32_t constant = chunk->code[offset + 1] | (chunk->code[offset + 2] << 8) |
-                      (chunk->code[offset + 3] << 16);
+  uint32_t constant = chunk->code[offset + 1] | (chunk->code[offset + 2] >> 8) |
+                      (chunk->code[offset + 3] >> 16);
   printf("%-16s %4d '", name, constant);
   printValue(chunk->constants.values[constant]);
   printf("'\n");
--- c/src/main.c
+++ c/src/main.c
@@ -1,6 +1,9 @@
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 int main(int argc, const char **argv) {
+  initVM();
+
   Chunk chunk;
   initChunk(&chunk);
 
@@ -8,9 +11,13 @@ int main(int argc, const char **argv) {
   writeChunk(&chunk, OP_CONSTANT, 69420, 67);
   writeChunk(&chunk, constant, 69420, 67);
 
-  writeChunk(&chunk, OP_RETURN, 69420, 67);
-  writeChunk(&chunk, OP_RETURN, 69421, 68);
-  disassembleChunk(&chunk, "test chunk");
+  int longConstant = addConstant(&chunk, 6.7);
+  writeChunk(&chunk, OP_CONSTANT_LONG, 69420, 68);
+  writeChunk(&chunk, longConstant, 69420, 69);
+
+  writeChunk(&chunk, OP_RETURN, 69420, 1000);
+  interpret(&chunk);
+  freeVM();
   freeChunk(&chunk);
   return 0;
 }
